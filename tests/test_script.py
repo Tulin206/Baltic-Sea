7c1e22b9bc7b0f76024b0213e3664267c0485196
IsratJahanTulin <tulin206@gmail.com>
2024-12-06 15:36:07 +0100

    rename lstm+cnn_(with-dvc).py to lstm_cnn_with_dvc.py and test_script is added for ci\cd pipeleine

---
+++ tests/test_script.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+from lstm_cnn_with_dvc import load_raw_data
+
+def test_load_raw_data():
+    train, test, mask = load_raw_data("C:/Users/Tim/Desktop/ISRAT/RostockUniversity/PyCharmProjects/data.npz")
+    assert train is not None
+    assert test is not None
+    assert mask is not None
+    assert isinstance(train, np.ndarray)
+    assert isinstance(test, np.ndarray)
+    assert isinstance(mask, np.ndarray)
